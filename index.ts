2de71c287ef8ea90a9d1ad9da08b050985d10ef4
cemalokten <cemalokten@gmail.com>
2023-09-16 08:03:26 +0100

    Add convert_to_operation fn

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@ export function subtract(n: number[]): number {
   for (let i = 1, l = n.length; i < l; i++) {
     val -= n[i];
   }
+
   return val;
 }
 
@@ -42,6 +43,35 @@ export function calculate(op: string, n: number[]): number | undefined {
       return divide(n);
   }
 }
+/**
+ * Array[String | Number] -> Array[String | Number]
+ * consume array and output array of operations and values in nested array
+ **/
+
+type StrNumAr = (string | number)[][];
+
+export function convert_to_operation(array: string[]): StrNumAr {
+  const operations: (string | number)[] = [];
+  const grouped_operations: (string | number)[][] = [];
+
+  for (let i = 0, n = array.length; i < n; i++) {
+    if (isNaN(array[i] as any)) {
+      operations.push(array[i]);
+    } else if (!isNaN(array[i] as any)) {
+      operations.push(Number(array[i]));
+    }
+  }
+  let idx = -1;
+  for (let j = 0, nn = operations.length; j < nn; j++) {
+    if (typeof operations[j] === "string") {
+      idx++;
+      grouped_operations.push([operations[j]]);
+    } else if (typeof operations[j] === "number") {
+      grouped_operations[idx].push(operations[j]);
+    }
+  }
+  return grouped_operations;
+}
 
 import express, { Request } from "express";
 
@@ -55,9 +85,8 @@ type Req = Request<{ operator: string; "0": string }, {}, {}>;
 app.get("/calculate/:operator/*", (req: Req, res, next) => {
   try {
     const operator = req.params.operator;
-    const values = req.params[0];
-    const array_of_values = values.split("/").map(Number);
-    /** TODO: Add arbitrary number of operators **/
+    const values = req.params[0].split("/");
+    // TODO: Fix this part
     const result = calculate(operator, array_of_values);
     res.status(200).json(result);
   } catch (error) {
